aeddf8bfadda8fab8378f6ada6ecb6e613fd8869
Christian Shtarkov <cshtarkov@gmail.com>
2015-10-12 15:53:24 +0100

    Initial commit

---
+++ reminder.py
@@ -0,0 +1,39 @@
+from os.path import expanduser, join
+from time import time
+
+class Py3status:
+    reminder_file = join(expanduser("~"), ".reminder")
+    cache_timeout = 5
+
+    def __init__(self):
+        pass
+
+    def kill(self, i3s_output_list, i3s_config):
+        pass
+
+    def put_reminder(self, i3s_output_list, i3s_config):
+        with open(self.reminder_file) as f:
+            full_text = ""
+            contents = f.readlines()
+            for line in contents:
+                line = line.rstrip()
+                full_text = str.format("{0} | {1}", full_text, line)
+            full_text = full_text[3:]
+            response = {
+                'full_text': full_text,
+                'cached_until': time() + self.cache_timeout
+            }
+            return response
+
+if __name__ == "__main__":
+    from time import sleep
+    r = Py3status()
+    config = {
+        'color_bad': '#FF0000',
+        'color_degraded': '#FFFF00',
+        'color_good': '#00FF00'
+    }
+    while True:
+        print(r.put_reminder([], config))
+        sleep(1)
+
